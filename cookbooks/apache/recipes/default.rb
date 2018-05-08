30313870f2b99bd2d1fa32383e0fc8e729f41349
Walker Wheeler <walker233@gmail.com>
2018-05-08 20:01:33 +0000

    Added basic apache cookbook.

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright (c) 2018 The Authors, All Rights Reserved.
+
+package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
